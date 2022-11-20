623c850ccdc10501f6dc48a6b656073746b56ae1
Joseph Emrick <josephemrick@Josephs-MacBook-Air.local>
2022-11-20 09:53:35 -0800

    added new route linked to new ejs

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -8,14 +8,6 @@ const router = express.Router();
 
 // Routes
 
-// Show routes
-router.get('/pokedex/:id', (req, res) => {
-    res.render('show.ejs', {
-        pokemonData: Pokemon,
-        pokemonStats: req.params.id
-    })
-})
-
 // Index routes
 router.get('/', (req, res) => {res.redirect('/pokedex')});
 
@@ -26,16 +18,22 @@ router.get('/pokedex', (req, res) => {
 })
 
 // New route
-router.get('/pokemon/new', (req, res) => {
+router.get('/pokedex/new', (req, res) => {
     res.render('new.ejs')
 })
 
 // Edit route
-router.get('/pokemon/edit', (req, res) => {
+router.get('/pokedex/edit', (req, res) => {
     res.render('edit.ejs')
 })
 
-//
+// Show routes
+router.get('/pokedex/:id', (req, res) => {
+    res.render('show.ejs', {
+        pokemonData: Pokemon,
+        pokemonStats: req.params.id
+    })
+})
 
 
 module.exports = router
