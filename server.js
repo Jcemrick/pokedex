0bed146dfec463f3326285ba500b7ef85102adce
Joseph Emrick <josephemrick@Josephs-MacBook-Air.local>
2022-11-20 07:16:12 -0800

    made inital server framework

---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies 
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const pokemonRouter = require('./controllers/pokemon')
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+
+// Middleware
+app.use(morgan('tiny'));
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+// Router use
+app.use('/', pokemonRouter)
+
+
+// Routes
+
+
+// Listener
+app.listen(PORT, () => console.log(`Listening on ${PORT}.`))
